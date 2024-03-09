ddd03cf9fe1455aecc2dad1c4e92980de92d3031
Morelowo <147094545+wojtekchwacinski@users.noreply.github.com>
2024-03-10 00:24:22 +0100

    dokońćzenie zad1 bbs

--- krypto/main.py
+++ krypto/main.py
@@ -2,13 +2,13 @@ import math
 import sympy
 # liczby pierwsze 
 p = 7451
-q = 1291
+q = 9767
 
 N = p * q
 
 #print(3%4)
 #print(p%4)
-#print(q%4)
+print(q%4)
 
 x_0 = sympy.randprime(1000,9000)
 def nwd(a, b):
@@ -27,7 +27,9 @@ out_bit = []
 x_1 = pow(x_0, 2, N)
 out.append(x_1)
 out_bit.append(out[-1] & 1)
-for i in range(20000):
+
+
+for i in range(19999):
     x = pow(out[-1], 2, N)
     out.append(x)
     out_bit.append(out[-1] & 1)
@@ -60,15 +62,90 @@ else:
 
 
 #test serii 
-#dla 1 
-seria1_1 = 0
-seria1_2 = 0
-seria1_3 = 0
-seria1_4 = 0
-seria1_5 = 0
-seria1_6 = 0
-def test_serii(bity):
-    for i in range(len(bity)):
-        if 
+#dla 1 i 0
+serie_1 = {}
+serie_0 = {}
+for i in range(0, 7):
+    serie_1[i] = 0
+    serie_0[i] = 0
 
+curr_series_1 = 0
+curr_series_0 = 0
+for b in out_bit:
+    if b == 1:
+        curr_series_1 += 1
+        if curr_series_0 > 6:
+            curr_series_0 = 6
+        serie_0[curr_series_0] += 1
+        curr_series_0 = 0
+    elif b == 0:
+        if curr_series_1 > 6:
+            curr_series_1 = 6
+        serie_1[curr_series_1] += 1
+        curr_series_1 = 0
+        curr_series_0 += 1
     
+print(f"Długości serii 1 ")
+for key in list(serie_1.keys())[1:]:
+    print(key, ":", serie_1[key])
+print(f"Długości serii 0 ")
+for key in list(serie_0.keys())[1:]:
+    print(key, ":", serie_0[key])
+
+#test długiej seerii
+long_series_1 = 0
+long_series_0 = 0
+test_pass = 0
+max_1 = 0
+max_0 = 0
+for bi in out_bit:
+    if bi == 1:
+        long_series_1 += 1
+        if long_series_1 > max_1:
+            max_1 = long_series_1
+        long_series_0 = 0
+        if long_series_1 >= 26:
+            test_pass = 1
+            break
+    elif bi == 0:
+        long_series_1 = 0
+        long_series_0 += 1
+        if long_series_0 > max_0:
+            max_0 = long_series_0
+        if long_series_0 >= 26:
+            test_pass = 1
+            break
+print(f"Najdłuższe ciągi 1 i 0: {max_1}, {max_0}")
+if test_pass == 0:
+    print("Obecny ciąg bitów przeszedł test długiej serii")
+else:
+    print("Obecny ciąg nie przeszedł próby długej serii")
+
+
+
+#test pokerowy
+
+sublists = [out_bit[i:i+4] for i in range(0, len(out_bit), 4)]
+licznosci = {}
+for i in range(0, 16):
+    licznosci[i] = 0
+
+for list in sublists:
+    wynik = 8 * list[0] + 4 * list[1] + 2 * list[2] + 1 * list[3]
+    #print(wynik)
+    licznosci[wynik] += 1
+
+print(licznosci)
+#oblcizenie sumy
+suma = 0
+for i in range(0, 16):
+    suma += licznosci[i] * licznosci[i]
+
+x = (16/5000) * suma - 5000
+
+if x > 2.16 and x < 46.17:
+    print(f"Test pokerowy zkończył się powodzeniem z wynikiem x: {x}")
+else:
+    print(f"Test pokerowy zkończył się niepowodzeniem z wynikiem x: {x}")
+
+
